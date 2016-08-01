30f033e1c61fd22bd25dd05093491d17f137e005
张贵广 <zhangguiguang@192.168.0.108>
2016-08-01 21:34:11 +0800

    新工程

---
+++ Euler-Project/Euler-Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Euler-Project
+//
+//  Created by 张贵广 on 8/1/16.
+//  Copyright © 2016 GG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

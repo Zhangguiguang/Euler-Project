f208ca5fd47ccb2bd585c3dc4e1052faac61d406
张贵广 <zhangguiguang@zhangguiguangdemac-mini.local>
2016-08-05 16:55:14 +0800

    29 create

--- Euler-Project/Problem029/main.m
+++ Euler-Project/Problem029/main.m
@@ -28,8 +28,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        
     }
     return 0;
 }
